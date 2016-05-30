35719ee531876b0e4d1eb7c5a11a6022cf785e9d
whitershade <whitershade34@gmail.com>
2016-05-30 19:26:49 +0300

    add directives

--- app/js/common.js
+++ app/js/common.js
@@ -7,6 +7,9 @@
   'use strict';
   let hideToggle;
   let inBasket;
+  let tasks = [
+//    { description: '1', deleted: false, done: true, hide: false, onchange: false }
+  ];
   const app = angular.module('toDoList', []);
   app.directive('currentDate', function () {
     return {
@@ -21,7 +24,7 @@
   app.directive('controlButtons', function () {
     return {
       restrict: 'E',
-      templateUrl: 'current-date.html',
+      templateUrl: 'control-buttons.html',
       controller: function () {
         this.hideToggle = hideToggle;
         this.inBasket = inBasket;
@@ -55,80 +58,55 @@
       controllerAs: 'btnCtrl'
     };
   });
-  app.controller('ButtonsController', function () {
-    this.hideToggle = hideToggle;
-    this.inBasket = inBasket;
-    this.addNewTask = function (descr) {
-      tasks.push({
-        description: descr,
-        deleted: false,
-        done: false,
-        hide: false
-      });
-      saveInLocalStorage();
-    };
-    this.toggleDone = function () {
-      this.hideToggle = hideToggle = !hideToggle;
-      tasks.forEach(function (item) {
-        item.done && hideToggle && (item.hide = true);
-        item.done && !hideToggle && (item.hide = false);
-      });
-      saveInLocalStorage();
-    };
-    this.toggleDeletedTasks = function () {
-      this.inBasket = inBasket = !inBasket;
-      tasks.forEach(function (item) {
-        item.hide = true;
-        inBasket && item.deleted && (item.hide = false);
-        !inBasket && !item.deleted && (item.hide = false);
-      });
-      saveInLocalStorage();
-    };
-  });
-  app.controller('TaskController', function () {
-    this.tasks = tasks;
-    this.changeTask = function (task, description) {
-      task.onchange = !task.onchange;
-      description && (task.description = description);
-      saveInLocalStorage();
-    }
-    this.toggleDone = function (task) {
-      task.done = !task.done;
-      hideToggle && (task.hide = true);
-      saveInLocalStorage();
-    };
-    this.deleteTask = function (task) {
-      task.deleted = true;
-      task.hide = true;
-      task.done = false;
-      saveInLocalStorage();
-    };
-    this.returnTask = function (task) {
-      task.deleted = false;
-      task.hide = true;
-      saveInLocalStorage();
-    }
-    this.finallyDeleteTask = function (task) {
-      let index;
-      let i = tasks.length - 1;
-      while (i >= 0) {
-        if (tasks[i].$$hashKey === task.$$hashKey) {
-          index = i;
-          break;
+  app.directive('tasksList', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'tasks-list.html',
+      controller: function () {
+        this.tasks = tasks;
+        this.changeTask = function (task, description) {
+          task.onchange = !task.onchange;
+          description && (task.description = description);
+          saveInLocalStorage();
         }
-        i--;
-      }
-      tasks.splice(index, 1);
-      saveInLocalStorage();
+        this.toggleDone = function (task) {
+          task.done = !task.done;
+          hideToggle && (task.hide = true);
+          saveInLocalStorage();
+        };
+        this.deleteTask = function (task) {
+          task.deleted = true;
+          task.hide = true;
+          task.done = false;
+          saveInLocalStorage();
+        };
+        this.returnTask = function (task) {
+          task.deleted = false;
+          task.hide = true;
+          saveInLocalStorage();
+        }
+        this.finallyDeleteTask = function (task) {
+          let index;
+          let i = tasks.length - 1;
+          while (i >= 0) {
+            if (tasks[i].$$hashKey === task.$$hashKey) {
+              index = i;
+              break;
+            }
+            i--;
+          }
+          tasks.splice(index, 1);
+          saveInLocalStorage();
+        };
+      },
+      controllerAs: 'taskCtrl'
     };
   });
-
   function saveInLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
     localStorage.setItem('hideToggle', hideToggle);
     localStorage.setItem('inBasket', inBasket);
   }
-
   function loadFromLocalStorage() {
     if (localStorage.getItem('tasks')) {
       tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -153,36 +131,5 @@
       inBasket = inBasket === 'true' ? true : false;
     }
   }
-
-  let tasks = [
-    {
-      description: '1',
-      deleted: false,
-      done: true,
-      hide: false,
-      onchange: false
-    },
-    {
-      description: '2',
-      deleted: false,
-      done: true,
-      hide: false,
-      onchange: false
-    },
-    {
-      description: '3',
-      deleted: false,
-      done: false,
-      hide: false,
-      onchange: false
-    },
-    {
-      description: '4',
-      deleted: true,
-      done: false,
-      hide: true,
-      onchange: false
-    }
-  ];
   loadFromLocalStorage();
 }());
