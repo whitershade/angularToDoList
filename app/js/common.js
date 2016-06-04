be6e871b548df773f1eea4cb364a5f0de7200bdc
whitershade <whitershade34@gmail.com>
2016-06-04 03:05:12 +0300

    add save factory and app.value

--- app/js/common.js
+++ app/js/common.js
@@ -5,13 +5,49 @@
 
 (function () {
   'use strict';
-  let hideToggle; // скрывать / показывать сделанные задачи
-  let inBasket; // показывать / скрывать удаленные задачи
   let tasks = [ // массив для хранения задач
 //    { description: '1', deleted: false, done: false, hide: false, onchange: false } --> так выглядит объект типа "задача", хранящийся в массиве
   ];
 
   const app = angular.module('toDoList', []); // инициализируем angular-приложение
+  app.value('appValues', {
+    hideToggle: false, // скрывать / показывать сделанные задачи
+    inBasket: false // показывать / скрывать удаленные задачи
+  });
+
+  app.factory('saveFactory', function (appValues) {
+    return {
+      saveInLocalStorage: function () {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        localStorage.setItem('hideToggle', appValues.hideToggle);
+        localStorage.setItem('inBasket', appValues.inBasket);
+      },
+      loadFromLocalStorage: function () {
+        if (localStorage.getItem('tasks')) { // если в local storage есть ключ tasks
+          tasks = JSON.parse(localStorage.getItem('tasks')); // получаем по ключу массив
+          tasks.forEach(function (item) { // для каждого элемента в массиве tasks 
+            item.$$hashKey = undefined; // устанавливаем hashKey = undefined (необходимо для избежание конфликтов при выводе задач)
+          });
+        }
+        appValues.hideToggle = localStorage.getItem('hideToggle'); // пытаемся считать значение для hide Toggle из Local Storage
+        if (!appValues.hideToggle) {
+          // если в local storage нет hideToggle (страница открыта впервые), то
+          appValues.hideToggle = false; // по умолчанию зададим ему false (значит, на него ещё не нажимали)
+        } else {
+          // если в local storage есть такой элемент, то
+          appValues.hideToggle = appValues.hideToggle === 'true' ? true : false; // если записана строка true, то преобразуем её в bool true, иначе в bool false
+        }
+        appValues.inBasket = localStorage.getItem('inBasket');
+        if (!appValues.inBasket) {
+          // если в local storage нет hideToggle (страница открыта впервые), то
+          appValues.inBasket = false; // по умолчанию зададим ему false (значит, на него ещё не нажимали)
+        } else {
+          // если в local storage есть такой элемент, то
+          appValues.inBasket = appValues.inBasket === 'true' ? true : false; // если записана строка true, то преобразуем её в bool true, иначе в bool false
+        }
+      }
+    }
+  });
 
   /* Директива для вывода текущей даты */
   app.directive('currentDate', function () {
@@ -26,13 +62,14 @@
   });
 
   /* Директива для кнопок упрвления */
-  app.directive('controlButtons', function () {
+  app.directive('controlButtons', function (saveFactory, appValues) {
     return {
       restrict: 'E', // only matches element name
       templateUrl: 'control-buttons.html', // где хранится html
-      controller: function () { // задаем контроллер
-        this.hideToggle = hideToggle; // задаем текущее значение hideToggle
-        this.inBasket = inBasket; // задаем текущее значение inBasket
+      controller: function (appValues) { // задаем контроллер
+        saveFactory.loadFromLocalStorage();
+        this.inBasket = appValues.inBasket; // задаем текущее значение inBasket
+        this.hideToggle = appValues.hideToggle; // задаем текущее значение hideToggle
         this.addNewTask = function (descr) { // добавляем новую задачу, на вход подается содержаение задачи 
           tasks.push({ // в массив задач добавляется новый объект с
             description: descr, //полученным при вызове функции описанием
@@ -41,24 +78,24 @@
             hide: false, // не скрыта
             onchange: false // не изменяется в текущий момент
           });
-          saveInLocalStorage(); // сохранить изменения в local storage
+          saveFactory.saveInLocalStorage(); // сохранить изменения в local storage
         };
         this.toggleDone = function () { // функция для переключения done/undone задачи
-          this.hideToggle = hideToggle = !hideToggle; // переключаем done/undone, глобальную и внутри котроллера
+          this.hideToggle = appValues.hideToggle = !appValues.hideToggle; // переключаем done/undone, глобальную и внутри котроллера
           tasks.forEach(function (item) { // для каждой задачи
-            item.done && hideToggle && (item.hide = true); // если задача сделана, и выбрано скрывать сделанные задачи, то скрываем
-            item.done && !hideToggle && (item.hide = false); // есил задача сделана, и выбрано показывать сделанные задачи, то показываем
+            item.done && appValues.hideToggle && (item.hide = true); // если задача сделана, и выбрано скрывать сделанные задачи, то скрываем
+            item.done && !appValues.hideToggle && (item.hide = false); // есил задача сделана, и выбрано показывать сделанные задачи, то показываем
           });
-          saveInLocalStorage(); // сохранить изменения в local storage 
+          saveFactory.saveInLocalStorage(); // сохранить изменения в local storage 
         };
         this.toggleDeletedTasks = function () {
-          this.inBasket = inBasket = !inBasket; // переключаем в корзине/не в корзине, глобальную и внутри котроллера
+          this.inBasket = appValues.inBasket = !appValues.inBasket; // переключаем в корзине/не в корзине, глобальную и внутри котроллера
           tasks.forEach(function (item) { // для каждой задачи
             item.hide = true; // скрываем каждую задачу
-            inBasket && item.deleted && (item.hide = false); // если в данный момент пользователь находится в корзине, и задача удалена, то показываем задачу
-            !inBasket && !item.deleted && (item.hide = false); // если пользователь не находится в корзине, и задача не удалена, то показываем её
+            appValues.inBasket && item.deleted && (item.hide = false); // если в данный момент пользователь находится в корзине, и задача удалена, то показываем задачу
+            !appValues.inBasket && !item.deleted && (item.hide = false); // если пользователь не находится в корзине, и задача не удалена, то показываем её
           });
-          saveInLocalStorage(); // сохраняем изменения в local storage
+          saveFactory.saveInLocalStorage(); // сохраняем изменения в local storage
         };
       },
       controllerAs: 'btnCtrl' // псевдоним для контроллера
@@ -66,32 +103,32 @@
   });
 
   /* Директива для списка задач */
-  app.directive('tasksList', function () {
+  app.directive('tasksList', function (saveFactory) {
     return {
       restrict: 'E', // only matches element name
       templateUrl: 'tasks-list.html', // где хранится html
-      controller: function () {
+      controller: function (appValues) {
         this.tasks = tasks; // получаем список задач
         this.changeTask = function (task, description) { // функция для изменения текущего содержания задачи
           task.onchange = !task.onchange; // переключаем onchange для задачи
           description && (task.description = description); // если в функцию передано содеражаение для записи в задачу, то записываем его
-          saveInLocalStorage(); // сохраняем изменения в local storage
+          saveFactory.saveInLocalStorage(); // сохраняем изменения в local storage
         }
         this.toggleDone = function (task) { // функция для изменения done/undone задачи
           task.done = !task.done; // переключаем done/undone для задачи
-          hideToggle && (task.hide = true); // если выбрано скрывать сделанные задачи, то скрываем только что отмеченную задачу
-          saveInLocalStorage(); // сохраняем изменения в local storage
+          appValues.hideToggle && (task.hide = true); // если выбрано скрывать сделанные задачи, то скрываем только что отмеченную задачу
+          saveFactory.saveInLocalStorage(); // сохраняем изменения в local storage
         };
         this.deleteTask = function (task) { // функция для перемещения задачи в корзину
           task.deleted = true; // задача является удаленной
           task.hide = true; // скрытой
           task.done = false; // и не выполненной
-          saveInLocalStorage(); // сохраняем изменения в local storage
+          saveFactory.saveInLocalStorage(); // сохраняем изменения в local storage
         };
         this.returnTask = function (task) { // функция для возвращени задачи из корзины
           task.deleted = false; // задача является не удаленной
           task.hide = true; // скрываем её из корзины
-          saveInLocalStorage(); // сохраняем изменения в local storage
+          saveFactory.saveInLocalStorage(); // сохраняем изменения в local storage
         }
         this.finallyDeleteTask = function (task) { // функция для окончательного удаления задачи
           if (confirm("Точно удалить задачу?")) { // запрос пользователю точно ли он хочет удалить задачу, если да, то переходим к удалению
@@ -105,47 +142,11 @@
               i--; // делаем следующий шаг
             }
             tasks.splice(index, 1); // удаляем задачу из массива задач
-            saveInLocalStorage(); // сохраняем измнения в local storage
+            saveFactory.saveInLocalStorage(); // сохраняем измнения в local storage
           }
         };
       },
       controllerAs: 'taskCtrl' // устанавливаем псевдоним для контроллера
     };
   });
-
-  /* Функция для сохрнанения изменений в local storage */
-  function saveInLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    localStorage.setItem('hideToggle', hideToggle);
-    localStorage.setItem('inBasket', inBasket);
-  }
-
-  /* Функция для загрузки данных из local storage */
-  function loadFromLocalStorage() {
-    if (localStorage.getItem('tasks')) { // если в local storage есть ключ tasks
-      tasks = JSON.parse(localStorage.getItem('tasks')); // получаем по ключу массив
-      tasks.forEach(function (item) { // для каждого элемента в массиве tasks 
-        item.$$hashKey = undefined; // устанавливаем hashKey = undefined (необходимо для избежание конфликтов при выводе задач)
-      });
-    }
-    hideToggle = localStorage.getItem('hideToggle'); // пытаемся считать значение для hide Toggle из Local Storage
-    if (!hideToggle) {
-      // если в local storage нет hideToggle (страница открыта впервые), то
-      hideToggle = false; // по умолчанию зададим ему false (значит, на него ещё не нажимали)
-    } else {
-      // если в local storage есть такой элемент, то
-      hideToggle = hideToggle === 'true' ? true : false; // если записана строка true, то преобразуем её в bool true, иначе в bool false
-    }
-    inBasket = localStorage.getItem('inBasket');
-    if (!inBasket) {
-      // если в local storage нет hideToggle (страница открыта впервые), то
-      inBasket = false; // по умолчанию зададим ему false (значит, на него ещё не нажимали)
-    } else {
-      // если в local storage есть такой элемент, то
-      inBasket = inBasket === 'true' ? true : false; // если записана строка true, то преобразуем её в bool true, иначе в bool false
-    }
-  }
-
-  /* При запуске получаем данные из local storage */
-  loadFromLocalStorage();
 }());
